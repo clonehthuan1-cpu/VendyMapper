613c92f9506d2d468fc70af6483766f701a7303c
clonehthuan1-cpu <clonehthuan1@gmail.com>
2026-07-17 14:45:12 +0700

    Add Color.kt with color definitions and gradients

---
+++ app/src/main/java/com/vendy/mapper/ui/theme/Color.kt
@@ -0,0 +1,27 @@
+package com.vendy.mapper.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DeepBlue = Color(0xFF0A0E27)
+val NavyBlue = Color(0xFF1A1F3A)
+val ElectricBlue = Color(0xFF0066FF)
+val NeonBlue = Color(0xFF00D4FF)
+val NeonPurple = Color(0xFF9945FF)
+val Silver = Color(0xFFC0C0C0)
+val Platinum = Color(0xFFE5E4E2)
+
+val NeonGreen = Color(0xFF00FF88)
+val NeonRed = Color(0xFFFF3366)
+val NeonOrange = Color(0xFFFF6B35)
+val NeonYellow = Color(0xFFFFD700)
+
+val DarkBackground = Color(0xFF070B1A)
+val CardBackground = Color(0xCC1A1F3A)
+val GlassBackground = Color(0x1AFFFFFF)
+
+val TextPrimary = Color(0xFFE8EAED)
+val TextSecondary = Color(0xFF9AA0A6)
+val TextAccent = Color(0xFF00D4FF)
+
+val BluePurpleGradient = listOf(Color(0xFF0066FF), Color(0xFF9945FF))
+val NeonGlowGradient = listOf(Color(0x0000D4FF), Color(0x4D00D4FF), Color(0x8000D4FF))
